8872c364fbd951c3e1dc40602a6ba9d08b3616cb
donghun <gnsia@naver.com>
2022-02-13 20:47:56 +0900

    버블링으로 처리하기

--- DOM/javascript2-event.js
+++ DOM/javascript2-event.js
@@ -1,14 +1,26 @@
+// Ex 2: 버블링을 이용한 사용자 이벤트 처리하기
+window.addEventListener("load", function() {
+    var section = this.document.querySelector("#section2");
+    // var imgs = section.querySelectorAll(".img");
+    var imgList = section.querySelector(".img-list");
+    var currentImg = section.querySelector(".current-img");
+
+    imgList.onclick = function(e) {
+        if(e.target.nodeName != "IMG") return;
+        currentImg.src = e.target.src;
+    }
+    
+});
+
 // 연습문제 1-선택된 레코드 삭제하기:event target
 window.addEventListener("load", function () {
     var section = this.document.querySelector("#section1-1");
-    var delButtons = section.querySelectorAll(".del-button");
-    for(var i=0; i<delButtons.length; i++) {
-        delButtons[i].onclick = function(e) {
-            var currentTr = e.target.parentElement.parentElement;
-            currentTr.remove();
-        };
+    var noticeList = section.querySelector(".notice-list");
+    noticeList.onclick = function(e) {
+        if(e.target.className != "del-button") return;
+        var tr = e.target.parentElement.parentElement;
+        tr.remove();
     }
-
 });
 
 
@@ -23,6 +35,4 @@ window.addEventListener("load", function() {
             currentImg.src = e.target.src;
         };
     }
-
-
 });
