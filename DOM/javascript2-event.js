d48b52848967f704ce0de7a05a8e12da9a3946e3
donghun <gnsia@naver.com>
2022-02-13 21:09:18 +0900

    stopPropagation 이용하여 부모이벤트 전파 막기

--- DOM/javascript2-event.js
+++ DOM/javascript2-event.js
@@ -1,3 +1,26 @@
+// Ex 3: 이벤트 버블링 멈추기
+window.addEventListener("load", function() {
+    var section = this.document.querySelector("#section3");
+    var imgList = section.querySelector(".img-list");
+    var addButton = section.querySelector(".add-button");
+    var currentImg = section.querySelector(".current-img");
+
+    imgList.onclick = function(e) {
+        console.log("imgList.onclick");
+        if(e.target.nodeName != "IMG") return;
+        currentImg.src = e.target.src;
+    }
+
+    addButton.onclick = function(e) {
+        e.stopPropagation();
+        console.log("addButton.onclick");
+        var img = document.createElement("img");
+        img.src = "../images/surf1.jpg"
+        currentImg.insertAdjacentElement("afterend", img);
+    }
+
+});
+
 // Ex 2: 버블링을 이용한 사용자 이벤트 처리하기
 window.addEventListener("load", function() {
     var section = this.document.querySelector("#section2");
@@ -9,7 +32,6 @@ window.addEventListener("load", function() {
         if(e.target.nodeName != "IMG") return;
         currentImg.src = e.target.src;
     }
-    
 });
 
 // 연습문제 1-선택된 레코드 삭제하기:event target
